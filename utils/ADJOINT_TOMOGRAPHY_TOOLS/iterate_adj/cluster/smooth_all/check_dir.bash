97160d6d59ffcfb160ffbb8f9b7b7cef422b719b
HarryYen <harry0109@gmail.com>
2025-03-10 10:58:48 +0800

    remove ./utils/

--- utils/ADJOINT_TOMOGRAPHY_TOOLS/iterate_adj/cluster/smooth_all/check_dir.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for eid in `ls -1 -d [0-9]*`; do
-
- echo "============ $eid ============"
- ls -1 $eid/inout_smooth/*mu_kernel_smooth.bin | wc
- ls -1 $eid/inout_smooth/*kappa_kernel_smooth.bin | wc
-
-done
